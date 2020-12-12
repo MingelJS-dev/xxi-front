899cdfaade4a82373adf5e15d2c8a3743f39b625
MingelJS-dev <mingel.js@gmail.com>
2020-12-12 04:20:41 -0300

    Add mobile view to product

--- src/app/products/ProductsTable.js
+++ src/app/products/ProductsTable.js
@@ -4,15 +4,16 @@ import { Link } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import * as utils from '../../utils.js'
 import Spinner from '../shared/Spinner.js';
+import Row from 'react-bootstrap/Row';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMicrochip, faDoorClosed, faHashtag } from '@fortawesome/free-solid-svg-icons';
+import { faDollarSign, faUtensils } from '@fortawesome/free-solid-svg-icons';
 
 import history from '../../history.js'
 
 import * as ProductReducer from '../../reducers/product.reducer.js'
 
-export default function ProductsTable() {
+export default function ProductsTable({mode = 'Table'}) {
   const products = useSelector(ProductReducer.getProducts)
   const isLoading = useSelector(ProductReducer.getIsLoading)
 
@@ -56,32 +57,68 @@ export default function ProductsTable() {
     // }
   ]
 
-  
-  return (
-    <table className={`table table-striped table-bordered table-hover mb-0`} >
-      <thead>
-        <tr>
-          {
-            headersData.map(item => (
-              <th key={item.field}>{item.label}</th>
-            ))
-          }
-        </tr>
-      </thead>
-
-      <tbody>
+  if (mode === "CardList") {
+    return (
+      <>
         {
-              products.map(item => (
-                <tr key={item.id}>
-                  {/* <td>*</td> */}
-                  <td><Link to={"/products/" + item.id + "/edit"}>{item.name}</Link></td>
-                  <td>${item.price}</td>
-                  <td> En desarrollo </td> 
+          products.map(item => (
+            <div
+              key={item.id}
+              onClick={() => history.push(`/products/${item.id}/edit`)}
+              className="card mb-2 shadow"
+            >
+              <Row className="card-body d-flex w-100 justify-content-between align-items-center">
+                <div className="ml-2">
+                  <p className="mb-1">
+                    <span className="d-inline-block mr-1" style={{ width: '20px' }}>
+                      <FontAwesomeIcon icon={faUtensils} />
+                    </span>
+                    <span>{item.name}</span>
+                  </p>
+                  <p className="mb-1">
+                    <span className="d-inline-block mr-1" style={{ width: '20px' }}>
+                      <FontAwesomeIcon icon={faDollarSign} />
+                    </span>
+                    <span>{item.price}</span>
+                  </p>
+                </div>
+                <div>
+                </div>
+              </Row>
+            </div>
+          ))
+        }
+      </>
 
-                </tr>
+    )
+  } else {
+    return (
+      <table className={`table table-striped table-bordered table-hover mb-0`} >
+        <thead>
+          <tr>
+            {
+              headersData.map(item => (
+                <th key={item.field}>{item.label}</th>
               ))
             }
-      </tbody>
-    </table>
-  )
+          </tr>
+        </thead>
+
+        <tbody>
+          {
+            products.map(item => (
+              <tr key={item.id}>
+                {/* <td>*</td> */}
+                <td><Link to={"/products/" + item.id + "/edit"}>{item.name}</Link></td>
+                <td>${item.price}</td>
+                <td> En desarrollo </td>
+
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    )
+  }
+
 }
